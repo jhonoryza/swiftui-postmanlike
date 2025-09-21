1252ea0bc7c7443e2789ca758392e5b5afe98156
fajar sidik priatna <jardik.oryza@gmail.com>
2025-09-22 06:28:02 +0700

    Initial Commit

---
+++ PostmanLike/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PostmanLike
+//
+//  Created by fajar on 22/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PostmanLike/PostmanLikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostmanLikeApp.swift
+//  PostmanLike
+//
+//  Created by fajar on 22/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PostmanLikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
